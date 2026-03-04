4676d33157f7b5cd92b51866bc4b85f0af157f01
Pinc3ng <vinss37926@gmail.com>
2026-03-04 19:20:17 +0700

    buat perubahan di bagian navbar

--- front-end/src/components/navbar.jsx
+++ front-end/src/components/navbar.jsx
@@ -9,7 +9,7 @@ const navbar = () => {
         <ul className="menu flex items-center justify-center gap-15">
             <li><a href="#">Home</a></li>
             <li><a href="#">About</a></li>
-            <li><a href="#">Contact</a></li>
+            <li><a href="#">Reserve</a></li>
         </ul>
 
         <div>
