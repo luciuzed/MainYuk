bd1bac4d442d3e07275c7dbb29065bf6ad2fa42a
Hsquad <alvarocaesar0807@gmail.com>
2026-03-05 15:17:46 +0700

    register form

--- front-end/src/page/LoginPage.jsx
+++ front-end/src/page/LoginPage.jsx
@@ -1,43 +1,77 @@
 // import React from 'react'
 import {useState} from 'react'
 import IMAGE from '../assets/ring.jpg'
-import { FaUser, FaLock} from "react-icons/fa";
+import { FaUser, FaLock, FaPhoneAlt, FaEye, FaEyeSlash  } from "react-icons/fa";
 
 const LoginPage = () => {
   const [role, setRole] = useState('Customer')
+  const [mode, setMode] = useState("login")
+  const [showPassword, setShowPassword] = useState(false)
   return (
     <div className="w-full h-[calc(100vh-80px)] flex ">
       <div className="w-1/2 flex flex-col pr-10 pt-5">
-        <div className="flex">
-          <button
-            onClick={() => setRole("Customer")}
-            className={`flex-1 pb-3 font-semibold ${
-              role === "Customer"
-                ? "text-primary border-b-2 border-primary"
-                : "text-gray-400"
-            }`} 
-          >
-            Customer
-          </button>
+        {mode === "login" && (
+          <div className="flex">
+            <button
+              onClick={() => setRole("Customer")}
+              className={`flex-1 pb-3 font-semibold ${
+                role === "Customer"
+                  ? "text-primary border-b-2 border-primary"
+                  : "text-gray-400"
+              }`} 
+            >
+              Customer
+            </button>
 
-          <button
-            onClick={() => setRole("Business")}
-            className={`flex-1 pb-3 font-semibold ${
-              role === "Business"
-                ? "text-primary border-b-2 border-primary"
-                : "text-gray-400"
-            }`}
-          >
-            Business
-          </button>
-        </div>
-
+            <button
+              onClick={() => setRole("Business")}
+              className={`flex-1 pb-3 font-semibold ${
+                role === "Business"
+                  ? "text-primary border-b-2 border-primary"
+                  : "text-gray-400"
+              }`}
+            >
+              Business
+            </button>
+          </div>
+        )}
         {/* <h1 className="text-2xl font-bold m-5 mb-2">Login as {role}</h1> */}
 
         <h1 className="font-semibold text-5xl mx-5 mt-10 mb-15 justify-center align-center flex">
-          Welcome back!
+          {mode === "login" ? "Welcome Back!" : "Create an Account"}
         </h1>
       
+        {mode === "register" && (
+          <div className="w-full flex flex-col items-center mb-6">
+
+            <div className="w-3/4">
+              <p className="mb-2">Register as:</p>
+
+              <div className="flex gap-8 mb-4">
+                <label className="flex items-center gap-2 cursor-pointer">
+                  <input type="radio" name="accountType" defaultChecked />
+                  <span>User</span>
+                </label>
+
+                <label className="flex items-center gap-2 cursor-pointer">
+                  <input type="radio" name="accountType" />
+                  <span>Admin</span>
+                </label>
+              </div>
+            </div>
+
+            {/* Full Name */}
+            <div className="relative w-3/4">
+              <FaUser className="absolute left-4 top-1/2 -translate-y-1/2 text-gray-400" />
+              <input
+                type="text"
+                placeholder="Full Name"
+                className="w-full pl-12 pr-4 py-3 border rounded-full focus:outline-none focus:ring-2 focus:ring-primary"
+              />
+            </div>
+
+          </div>
+        )}
 
         <div className="w-full flex justify-center">
           <div className="relative w-3/4 mb-4">
@@ -53,34 +87,93 @@ const LoginPage = () => {
           </div>
         </div>
 
-        <div className="w-full flex justify-center">
-          <div className="relative w-3/4 mb-6">
-            
-            <FaLock className="absolute left-4 top-1/2 -translate-y-1/2 text-gray-400" />
-            
-            <input
-              type="password"
-              placeholder="Password"
-              className="w-full pl-12 pr-4 py-3 border rounded-full focus:outline-none focus:ring-2 focus:ring-primary"
-            />
-            
+        {mode === "register" && (
+          <div className="w-full flex justify-center">
+            <div className="relative w-3/4 mb-4">
+              <FaPhoneAlt className="absolute left-4 top-1/2 -translate-y-1/2 text-gray-400" />
+              <input
+                type="tel"
+                placeholder="+62"
+                className="w-full pl-12 pr-4 py-3 border rounded-full focus:outline-none focus:ring-2 focus:ring-primary"
+              />
+            </div>
           </div>
-        </div>
+        )}
 
-          <div className="flex justify-center">
-            <button className="w-3/4 bg-primary text-white py-3 rounded-full font-semibold cursor-pointer hover:opacity-90 transition">
-              Continue
-            </button>
+        {mode === "login" && (
+          <div className="w-full flex justify-center">
+            <div className="relative w-3/4 mb-6">
+
+              <FaLock className="absolute left-4 top-1/2 -translate-y-1/2 text-gray-400" />
+
+              <input
+                type={showPassword ? "text" : "password"}
+                placeholder="Password"
+                className="w-full pl-12 pr-12 py-3 border rounded-full focus:outline-none focus:ring-2 focus:ring-primary"
+              />
+
+              {showPassword ? (
+                <FaEyeSlash
+                  onClick={() => setShowPassword(false)}
+                  className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-400 cursor-pointer"
+                />
+              ) : (
+                <FaEye
+                  onClick={() => setShowPassword(true)}
+                  className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-400 cursor-pointer"
+                />
+              )}
+
+            </div>
           </div>
+        )}
+
+        {mode === "register" && (
+          <div className="w-full flex justify-center">
+            <div className="w-3/4 flex gap-4 mb-6">
+
+              <div className="relative w-1/2">
+                <FaLock className="absolute left-4 top-1/2 -translate-y-1/2 text-gray-400" />
+                <input
+                  type="password"
+                  placeholder="Password"
+                  className="w-full pl-12 pr-4 py-3 border rounded-full focus:outline-none focus:ring-2 focus:ring-primary"
+                />
+              </div>
+
+              <div className="relative w-1/2">
+                <FaLock className="absolute left-4 top-1/2 -translate-y-1/2 text-gray-400" />
+                <input
+                  type="password"
+                  placeholder="Confirm Password"
+                  className="w-full pl-12 pr-4 py-3 border rounded-full focus:outline-none focus:ring-2 focus:ring-primary"
+                />
+              </div>
+
+            </div>
+          </div>
+        )}
+
+        <div className="flex justify-center">
+          <button className="w-3/4 bg-primary text-white py-3 rounded-full font-semibold cursor-pointer hover:opacity-90 transition">
+            Continue
+          </button>
+        </div>
 
           <div className="w-full flex justify-center mt-4">
-            <button className="text-primary font-medium hover:underline cursor-pointer">
-              Don’t have an account? Register
+            <button
+              onClick={() => {
+                setMode(mode === "login" ? "register" : "login");
+                setRole("Customer");
+              }}
+              className="text-primary font-medium hover:underline cursor-pointer"
+            >
+              {mode === "login"
+                ? "Don’t have an account? Register"
+                : "Already have an account? Login"}
             </button>
           </div>
         </div>
-
-
 
         <div className="w-1/2 flex justify-center items-center ">
           <img 
